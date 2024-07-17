55ee4c591664a4708c0bf5d9f71571d87b0ab53e
Balázs Kis <uibali@gmail.com>
2024-07-17 06:32:31 +0200

    error handling fix for config reading

--- DynamicDnsClient/Configuration/AppConfig.cs
+++ DynamicDnsClient/Configuration/AppConfig.cs
@@ -1,16 +1,15 @@
-﻿namespace DynamicDnsClient.Configuration
+﻿namespace DynamicDnsClient.Configuration;
+
+public sealed record AppConfig
 {
-    public sealed record AppConfig
-    {
-        public const string DomainNamePlaceholder = "{Domain}";
-        public const string HostNamePlaceholder = "{Host}";
-        public const string DnsApiSecretPlaceholder = "{Secret}";
-        public const string UpdatedIpPlaceholder = "{NewIp}";
+    public const string DomainNamePlaceholder = "{Domain}";
+    public const string HostNamePlaceholder = "{Host}";
+    public const string DnsApiSecretPlaceholder = "{Secret}";
+    public const string UpdatedIpPlaceholder = "{NewIp}";
     
-        public string[]? IpProviderUrls { get; init; }
-        public string? DomainName { get; init; }
-        public string[]? Hosts { get; init; }
-        public string? DnsApiUrlTemplate { get; init; }
-        public string? DnsApiSecret { get; init; }
-    }
+    public string[]? IpProviderUrls { get; init; }
+    public string? DomainName { get; init; }
+    public string[]? Hosts { get; init; }
+    public string? DnsApiUrlTemplate { get; init; }
+    public string? DnsApiSecret { get; init; }
 }
--- DynamicDnsClient/Configuration/ConfigReader.cs
+++ DynamicDnsClient/Configuration/ConfigReader.cs
@@ -1,86 +1,86 @@
 ﻿using DynamicDnsClient.Logging;
 
-namespace DynamicDnsClient.Configuration
-{
-    public static class ConfigReader
-    {
-        private const string ConfigPath = ".config";
-        private const char ConfigValueSeparator = '=';
-        private const char ConfigListSeparator = ',';
+namespace DynamicDnsClient.Configuration;
 
-        public static AppConfig? ReadConfiguration()
+public static class ConfigReader
+{
+    private const string ConfigPath = ".config";
+    private const char ConfigValueSeparator = '=';
+    private const char ConfigListSeparator = ',';
+
+    public static AppConfig? ReadConfiguration()
+    {
+        AppConfig configuration;
+
+        try
         {
             var items = File.ReadAllLines(ConfigPath);
-            AppConfig configuration;
 
-            try
+            var configDictionary = items
+                .Select(i => i.Split(ConfigValueSeparator))
+                .ToDictionary(i => i[0], i => i[1]);
+
+            configuration = new AppConfig
             {
-                var configDictionary = items
-                    .Select(i => i.Split(ConfigValueSeparator))
-                    .ToDictionary(i => i[0], i => i[1]);
-
-                configuration = new AppConfig
-                {
-                    IpProviderUrls = configDictionary
-                        .GetValueOrDefault(nameof(AppConfig.IpProviderUrls))
-                        ?.Split(ConfigListSeparator),
-                    DomainName = configDictionary.GetValueOrDefault(nameof(AppConfig.DomainName)),
-                    Hosts = configDictionary
-                        .GetValueOrDefault(nameof(AppConfig.Hosts))
-                        ?.Split(ConfigListSeparator),
-                    DnsApiUrlTemplate = configDictionary.GetValueOrDefault(nameof(AppConfig.DnsApiUrlTemplate)),
-                    DnsApiSecret = configDictionary.GetValueOrDefault(nameof(AppConfig.DnsApiSecret))
-                };
-            }
-            catch (Exception ex)
-            {
-                ConsoleLogger.LogError($"The configuration was not readable. {ex.GetType().Name}: {ex.Message}");
-                return null;
-            }
-
-            var (isValid, problems) = ValidateConfiguration(configuration);
-            if (isValid)
-            {
-                return configuration;
-            }
-
-            ConsoleLogger.LogError($"Required item(s) are missing from the configuration: {problems}");
+                IpProviderUrls = configDictionary
+                    .GetValueOrDefault(nameof(AppConfig.IpProviderUrls))
+                    ?.Split(ConfigListSeparator),
+                DomainName = configDictionary.GetValueOrDefault(nameof(AppConfig.DomainName)),
+                Hosts = configDictionary
+                    .GetValueOrDefault(nameof(AppConfig.Hosts))
+                    ?.Split(ConfigListSeparator),
+                DnsApiUrlTemplate = configDictionary.GetValueOrDefault(nameof(AppConfig.DnsApiUrlTemplate)),
+                DnsApiSecret = configDictionary.GetValueOrDefault(nameof(AppConfig.DnsApiSecret))
+            };
+        }
+        catch (Exception ex)
+        {
+            ConsoleLogger.LogError($"The configuration was not readable. {ex.GetType().Name}: {ex.Message}");
             return null;
         }
 
-        private static (bool isValid, string? problems) ValidateConfiguration(AppConfig appConfig)
+        var (isValid, problem) = ValidateConfiguration(configuration);
+        if (isValid)
         {
-            var problemsBuilder = new List<string>();
-
-            if (appConfig.IpProviderUrls is null || appConfig.IpProviderUrls.Length == 0)
-            {
-                problemsBuilder.Add("IP provider URL(s) not specified");
-            }
-        
-            if (string.IsNullOrWhiteSpace(appConfig.DomainName))
-            {
-                problemsBuilder.Add("Domain name not specified");
-            }
-
-            if (appConfig.Hosts is null || appConfig.Hosts.Length == 0)
-            {
-                problemsBuilder.Add("Host(s) are not specified");
-            }
-
-            if (string.IsNullOrWhiteSpace(appConfig.DnsApiUrlTemplate))
-            {
-                problemsBuilder.Add("DNS API URL template not specified");
-            }
-
-            if (appConfig.DnsApiUrlTemplate?.Contains(AppConfig.DnsApiSecretPlaceholder) == true &&
-                string.IsNullOrWhiteSpace(appConfig.DnsApiSecret))
-            {
-                problemsBuilder.Add("DNS API secret is needed but not specified");
-            }
-
-            var problems = problemsBuilder.Count > 0 ? string.Join(", ", problemsBuilder) : null;
-
-            return (problems is null, problems);
+            return configuration;
         }
+
+        ConsoleLogger.LogError($"Required item(s) are missing from the configuration: {problem}");
+        return null;
+    }
+
+    private static (bool isValid, string? problem) ValidateConfiguration(AppConfig appConfig)
+    {
+        var problems = new List<string>();
+
+        if (appConfig.IpProviderUrls is null || appConfig.IpProviderUrls.Length == 0)
+        {
+            problems.Add("IP provider URL(s) not specified");
+        }
+        
+        if (string.IsNullOrWhiteSpace(appConfig.DomainName))
+        {
+            problems.Add("Domain name not specified");
+        }
+
+        if (appConfig.Hosts is null || appConfig.Hosts.Length == 0)
+        {
+            problems.Add("Host(s) are not specified");
+        }
+
+        if (string.IsNullOrWhiteSpace(appConfig.DnsApiUrlTemplate))
+        {
+            problems.Add("DNS API URL template not specified");
+        }
+
+        if (appConfig.DnsApiUrlTemplate?.Contains(AppConfig.DnsApiSecretPlaceholder) == true &&
+            string.IsNullOrWhiteSpace(appConfig.DnsApiSecret))
+        {
+            problems.Add("DNS API secret is needed but not specified");
+        }
+
+        var problem = problems.Count > 0 ? string.Join(", ", problems) : null;
+
+        return (problem is null, problem);
     }
 }
